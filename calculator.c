d8fd9b28f27e09ba1bbeb076d577c8d32d725c53
decqart <112713369+decqart@users.noreply.github.com>
2022-12-04 02:08:48 +0100

    calculator: added better mouse logic

--- calculator.c
+++ calculator.c
@@ -6,24 +6,26 @@
 int max_y = 0, max_x = 0;
 int submax_y, submax_x;
 int mouse_y, mouse_x;
-int num = 0, numb = 0, d_answer = 0;
+long num = 0, numb = 0, d_answer = 0;
+int ch;
 int quit = 0;
 char answer[100] = {0};
 
-void draw_keypad()
+void draw_keypad(void)
 {
     WINDOW *subwindow = subwin(stdscr, max_y-4, max_x-4, 3, 2);
     box(subwindow, ACS_VLINE, ACS_HLINE);
     getmaxyx(subwindow, submax_y, submax_x);
-    mvwhline(subwindow, submax_y/4,   1, ACS_HLINE, max_x-6);
-    mvwhline(subwindow, submax_y/2,   1, ACS_HLINE, max_x-6);
+    mvwhline(subwindow, submax_y/5,   1, ACS_HLINE, max_x-6);
+    mvwhline(subwindow, submax_y*3/7, 1, ACS_HLINE, max_x-6);
+    mvwhline(subwindow, submax_y*3/5, 1, ACS_HLINE, max_x-6);
     mvwhline(subwindow, submax_y*3/4, 1, ACS_HLINE, max_x-6);
     mvwvline(subwindow, 1, submax_x/4,   ACS_VLINE, submax_y-2);
     mvwvline(subwindow, 1, submax_x/2,   ACS_VLINE, submax_y-2);
     mvwvline(subwindow, 1, submax_x*3/4, ACS_VLINE, submax_y-2);
 }
 
-void calculate()
+void calculate(void)
 {
     int symbol = strcspn(answer, "+-*/");
     char numstr[20];
@@ -36,37 +38,54 @@ void calculate()
     case '+':
         numbstr = strchr(answer, '+');
         numbstr[0] = ' ';
-        numb = atoi(numbstr);
+        numb = atol(numbstr);
         d_answer = num + numb;
         break;
     case '-':
         numbstr = strchr(answer, '-');
         numbstr[0] = ' ';
-        numb = atoi(numbstr);
+        numb = atol(numbstr);
         d_answer = num - numb;
         break;
     case '*':
         numbstr = strchr(answer, '*');
         numbstr[0] = ' ';
-        numb = atoi(numbstr);
+        numb = atol(numbstr);
         d_answer = num * numb;
         break;
     case '/':
         numbstr = strchr(answer, '/');
         numbstr[0] = ' ';
-        numb = atoi(numbstr);
+        numb = atol(numbstr);
         d_answer = num / numb;
         break;
     default:
         break;
     }
-    sprintf(answer, "%d", d_answer);
+    sprintf(answer, "%ld", d_answer);
     clear();
 }
 
-void key_events()
+int mouse_input_box(int x1, int y1, int x2, int y2)
 {
-    switch (getch())
+    if (x1 < mouse_x && mouse_x < x2 &&
+        y1 < mouse_y && mouse_y < y2)
+        return 1;
+    return 0;
+}
+
+void key_events(void)
+{
+    ch = getch();
+
+    if (mouse_input_box(2, 3, 11, 6))
+    {
+        ch = 'c';
+    }
+
+    /* TODO: add button locations */
+    
+    switch (ch)
     {
     case '1':
         strcat(answer, "1");
@@ -122,6 +141,10 @@ void key_events()
         answer[strlen(answer)-1] = 0;
         clear();
         break;
+    case 'c':
+        /* CLEAR */
+        answer[0] = '\0';
+        break;
     case 'q':
         quit = 1;
         break;
@@ -132,9 +155,14 @@ void key_events()
     if (getmouse(&event) == OK)
     {
         clear();
-        mvprintw(4, 2, "Mouse at row=%d, column=%d", event.y, event.x);
+        mvprintw(4, 2, "Mouse at row=%d, column=%d\nbstate=%d", event.y, event.x, event.bstate);
         mouse_y = event.y;
         mouse_x = event.x;
+        if (event.bstate == 2)
+        {
+            mouse_y = -1;
+            mouse_x = -1;
+        }
     }
 }
 
@@ -145,7 +173,7 @@ int main(void)
     raw();
     curs_set(0);
     noecho();
-    mousemask(ALL_MOUSE_EVENTS, NULL);
+    mousemask(BUTTON1_PRESSED | BUTTON1_RELEASED, NULL);
     keypad(stdscr, 1);
 
     while (!quit)
