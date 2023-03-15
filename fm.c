6bc94f28c1d0dab14285b66322b5f213d32e4e57
decqart <112713369+decqart@users.noreply.github.com>
2023-03-15 03:54:51 +0100

    fm: removed unneeded code adn started showing '.' directory

--- fm.c
+++ fm.c
@@ -49,9 +49,9 @@ void print_dirs(WINDOW *win, StrArray array)
     int count = 1;
 
     //skipping first one as it is "." the directory itself
-    for (int i = 1; array.value[i] != NULL; ++i)
+    for (int i = 0; array.value[i] != NULL; ++i)
     {
-        if (cursor.pos == i-1)
+        if (cursor.pos == i)
         {
             wattron(win, COLOR_PAIR(1));
             selected = array.value[i];
@@ -60,10 +60,10 @@ void print_dirs(WINDOW *win, StrArray array)
         size_t len = strlen(array.value[i]);
         len = MIN(width, len);
 
-        mvwaddstr(win, i-movement, 1, array.value[i]);
-        fill_space(win, i-movement, len+1, width+1);
+        mvwaddstr(win, i-movement+1, 1, array.value[i]);
+        fill_space(win, i-movement+1, len+1, width+1);
 
-        if (cursor.pos == i-1)
+        if (cursor.pos == i)
             wattroff(win, COLOR_PAIR(1));
     }
     cursor.end = count-2;
@@ -75,14 +75,18 @@ void create_windows(void)
     right = subwin(stdscr, max_y, max_x/2, 0, max_x/2);
 }
 
+void delete_windows(void)
+{
+    delwin(left);
+    delwin(right);
+}
+
 void draw_borders(void)
 {
     box(left, ACS_VLINE, ACS_HLINE);
     box(right, ACS_VLINE, ACS_HLINE);
     mvprintw(max_y-1, 0, "selected = %d-%d", cursor.pos, cursor.end);
     //mvprintw(max_y-1, 0, "file = %s", selected);
-    wrefresh(left);
-    wrefresh(right);
 }
 
 void read_dir(const char *dir_path, WINDOW *win)
@@ -146,8 +150,7 @@ void input_events(void)
         break;
     }
 
-    wclear(left);
-    wclear(right);
+    clear();
 }
 
 int main(int argc, char **argv)
@@ -157,7 +160,7 @@ int main(int argc, char **argv)
         puts("usage: fm [dir]");
         return 1;
     }
-    int height = 0, width = 0;
+
     initscr();
     raw();
     curs_set(0);
@@ -175,13 +178,7 @@ int main(int argc, char **argv)
         getmaxyx(stdscr, max_y, max_x);
         create_windows();
         input_events();
-
-        if (max_x != width || max_y != height)
-        {
-            width = max_x;
-            height = max_y;
-            clear();
-        }
+        delete_windows();
     }
 
     stra_destroy(&files);
