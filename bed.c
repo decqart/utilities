5549b594d66758856917c3ad623ca6f1e16e229d
decqart <112713369+decqart@users.noreply.github.com>
2023-03-16 16:08:18 +0100

    bed: fixed segfault when inserting too many newlines

--- bed.c
+++ bed.c
@@ -18,7 +18,7 @@ typedef struct {
 } Line;
 
 typedef struct {
-    Line *line;
+    Line *idx;
     size_t count;
     size_t used;
 } Lines;
@@ -28,8 +28,7 @@ typedef struct {
     size_t filled;
     size_t size;
     Pos pos;
-    Line *line;
-    size_t line_count;
+    Lines lines;
 } Text;
 
 inline size_t get_newline_count(const char *str)
@@ -41,13 +40,29 @@ inline size_t get_newline_count(const char *str)
     return count;
 }
 
+Lines init_lines(size_t line_count)
+{
+    size_t size = line_count * 2;
+    size_t used = line_count;
+    Line *line = malloc(size * sizeof(Line));
+    return (Lines) { line, size, used };
+}
+
+void update_lines(Lines *line)
+{
+    if (line->count == line->used)
+    {
+        line->count <<= 1;
+        line->idx = realloc(line->idx, line->count * sizeof(Line));
+    }
+}
+
 Text init_text(char *cont)
 {
     char *buffer = NULL;
-    Line *line = NULL;
     size_t filled_size = 0;
     size_t buffer_size = 0;
-    size_t line_count = 0;
+    Lines lines;
 
     if (cont != NULL)
     {
@@ -57,8 +72,7 @@ Text init_text(char *cont)
         memcpy(buffer, cont, cont_len+1);
         buffer_size = cont_len;
 
-        line = malloc(nl_count * 2 * sizeof(Line));
-        line_count = nl_count * 2;
+        lines = init_lines(nl_count);
 
         size_t line_len = 0;
         size_t line_num = 0;
@@ -66,7 +80,7 @@ Text init_text(char *cont)
         {
             if (buffer[i] == '\n')
             {
-                line[line_num].len = line_len;
+                lines.idx[line_num].len = line_len;
                 line_num++;
                 line_len = 0;
                 continue;
@@ -80,24 +94,19 @@ Text init_text(char *cont)
         buffer = malloc(128 * sizeof(char));
         buffer[0] = '\0';
         buffer_size = 128;
-        line = malloc(20 * sizeof(Line));
-        line_count = 20;
+        lines = init_lines(32);
         for (int i = 0; i < 20; ++i)
-            line[i].len = 0;
+            lines.idx[i].len = 0;
     }
 
-    return (Text) {
-        buffer, filled_size,
-        buffer_size, (Pos) { 0, 0 },
-        line, line_count
-    };
+    return (Text) { buffer, filled_size, buffer_size, (Pos) { 0, 0 }, lines };
 }
 
 size_t get_actual_pos(Text *text)
 {
     size_t pos = 0;
     for (int i = 0; i < text->pos.y; ++i)
-        pos += text->line[i].len;
+        pos += text->lines.idx[i].len;
 
     return pos+text->pos.x;
 }
@@ -107,19 +116,19 @@ void move_up(Text *text)
     if (text->pos.y != 0)
     {
         text->pos.y--;
-        if (text->line[text->pos.y].len-1 < text->pos.x)
-            text->pos.x = text->line[text->pos.y].len-1;
+        if (text->lines.idx[text->pos.y].len-1 < text->pos.x)
+            text->pos.x = text->lines.idx[text->pos.y].len-1;
     }
 }
 
 void move_down(Text *text)
 {
     //FIXIT: replace cursor based on if line has '\n'
-    if (text->line[text->pos.y+1].len != 0)
+    if (text->lines.idx[text->pos.y+1].len != 0)
     {
         text->pos.y++;
-        if (text->line[text->pos.y].len-1 <= text->pos.x)
-            text->pos.x = text->line[text->pos.y].len-1;
+        if (text->lines.idx[text->pos.y].len-1 <= text->pos.x)
+            text->pos.x = text->lines.idx[text->pos.y].len-1;
     }
 }
 
@@ -128,7 +137,7 @@ void move_left(Text *text)
     if (text->pos.x == 0 && text->pos.y != 0)
     {
         text->pos.y--;
-        text->pos.x = text->line[text->pos.y].len;
+        text->pos.x = text->lines.idx[text->pos.y].len;
     }
     if (text->pos.x != 0)
         text->pos.x--;
@@ -142,12 +151,14 @@ void move_right(Text *text)
         text->pos.y++;
         text->pos.x = 0;
     }
-    else if (text->line[text->pos.y].len != text->pos.x)
+    else if (text->lines.idx[text->pos.y].len != text->pos.x)
         text->pos.x++;
 }
 
 void insert_char(Text *buf, char ch)
 {
+    Line *line = buf->lines.idx;
+
     buf->filled++;
     if (buf->filled == buf->size)
     {
@@ -159,13 +170,15 @@ void insert_char(Text *buf, char ch)
     size_t pos = get_actual_pos(buf);
     if (ch == '\n')
     {
-        for (int i = buf->line_count; buf->pos.y+1 < i; --i)
-            buf->line[i].len = buf->line[i-1].len;
+        for (int i = buf->lines.count; buf->pos.y+1 < i; --i)
+            line[i].len = line[i-1].len;
 
-        buf->line[buf->pos.y+1].len = buf->line[buf->pos.y].len - buf->pos.x;
-        buf->line[buf->pos.y].len = buf->pos.x;
+        line[buf->pos.y+1].len = line[buf->pos.y].len - buf->pos.x;
+        line[buf->pos.y].len = buf->pos.x;
+        buf->lines.used++;
+        update_lines(&buf->lines);
     }
-    buf->line[buf->pos.y].len++;
+    line[buf->pos.y].len++;
     for (size_t i = buf->filled; pos < i; --i)
         buf->content[i] = buf->content[i-1];
 
@@ -174,20 +187,22 @@ void insert_char(Text *buf, char ch)
 
 void delete_char(Text *buf)
 {
-    if (buf->pos.x == 0 && buf->pos.y == 0) return;
-    move_left(buf);
-    buf->filled--;
+    Line *line = buf->lines.idx;
+
     size_t pos = get_actual_pos(buf);
+    if (buf->content[pos] == '\0') return;
+    buf->filled--;
 
     if (buf->content[pos] == '\n')
     {
-        buf->line[buf->pos.y].len += buf->line[buf->pos.y+1].len-1;
+        line[buf->pos.y].len += line[buf->pos.y+1].len-1;
 
-        for (int i = buf->pos.y+1; i < buf->line_count; ++i)
-            buf->line[i].len = buf->line[i+1].len;
+        for (int i = buf->pos.y+1; i < buf->lines.count; ++i)
+            line[i].len = line[i+1].len;
+        buf->lines.used--;
     }
     else
-        buf->line[buf->pos.y].len--;
+        buf->lines.idx[buf->pos.y].len--;
 
     for (size_t i = pos; i < buf->filled; ++i)
         buf->content[i] = buf->content[i+1];
@@ -198,9 +213,9 @@ void delete_char(Text *buf)
 void free_text(Text *buf)
 {
     free(buf->content);
-    free(buf->line);
+    free(buf->lines.idx);
     buf->content = NULL;
-    buf->line = NULL;
+    buf->lines.idx = NULL;
     buf->size = 0;
     buf->filled = 0;
     buf->pos = (Pos) { 0, 0 };
@@ -228,9 +243,12 @@ void key_events(Text *text)
     case KEY_RIGHT:
         move_right(text);
         break;
-    case 127:// DELETE
+    case KEY_DC:// DELETE
+        delete_char(text);
         break;
     case KEY_BACKSPACE:
+        if (text->pos.x == 0 && text->pos.y == 0) return;
+        move_left(text);
         delete_char(text);
         break;
     case 27: // ESCAPE
@@ -265,10 +283,10 @@ int main(void)
         waddstr(text_win, text.content);
         mvprintw(max_y-1, 0, "pos = %ld, %ld", text.pos.x, text.pos.y);
         mvprintw(max_y-2, 0, "size = %ld", text.size);
-        mvprintw(max_y-3, 0, "line len = %ld", text.line[text.pos.y].len);
+        mvprintw(max_y-3, 0, "line len = %ld", text.lines.idx[text.pos.y].len);
+        mvprintw(max_y-1, 12, "line size = %ld", text.lines.count);
 
         move(text.pos.y, text.pos.x+1);
-        wrefresh(text_win);
         key_events(&text);
 
         delwin(text_win);
